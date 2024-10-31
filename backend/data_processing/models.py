0ebee4f17113a071d79c08baef5184c7eb980850
Ramil Srivastava <ramilsrivastava@gmail.com>
2024-10-31 16:00:29 +1100

    init and model created

---
+++ backend/data_processing/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class File(models.Model):
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to='files/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
